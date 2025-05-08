afbaec95ef7de491f08ce9295aca0f992878dabc
Alexander Leonida <alexander.leonida@whoweplayfor.org>
2025-05-08 13:45:31 -0700

    picture documentation

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Azure MySQL, C#, .NET visualization SWE personal project!
 
 README is basically on home page of website. Pictures (if Azure database or server / domain was shut down due to cost) will be located [here](./documentation/)
 
+If this is the case, reach out to aleonidab@gmail.com to see project run. 
+
 API endpoint pull from https://developer.sportradar.com/, all usernames / passwords located either in Azure or in .env file. 
 
 No docker container for this project.
--- app/obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
+++ app/obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd60f487f0bda74aadd833d32454b3b6ac7ab47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c4c12c3f7c58ac5b8c0a8758d0161066dfcf101")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
